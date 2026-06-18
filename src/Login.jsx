d3d6b79a5ff16fb105a2c321f1642669d5364c12
vishal <vishaleditz007@gmail.com>
2026-06-18 12:51:11 +0530

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from './/assets/logo-black.png'
+import './css/Login.css'
+import { loginurl } from './Spotify.jsx'
+const Login = () => {
+  return (
+    <div className=' login'>
+      {/* logo */}
+      <img src={Logo} alt="Spotify Logo" />
+
+      {/* button */}
+      <a href={loginurl} className='bg-green-500 rounded-lg  '>Login with Spotify</a>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Spotify.jsx
@@ -0,0 +1,16 @@
+ export const authEndpoint = 'https://accounts.spotify.com/authorize';
+ //const redirecturi=" https://handrail-bribe-unlucky.ngrok-free.dev ";
+//const redirecturi = 'https://127.0.0.1:5173/';
+const redirecturi = 'https://spotify-clone-lyart.vercel.app/';
+ const clientId="de4631e9b1a24f9b91faeea2c94bf51f";
+ const scopes = [
+"user-read-currently-playing",
+"user-read-recently-played",
+"user-read-playback-state",
+"user-top-read",
+"user-modify-playback-state",
+ ];
+//%20 for ascii and respomse_type=token is used to send as a string and get the access token in the url rytaaa
+ //export const loginurl='${authEndpoint}?client_id=${clientId}&scope=${scopes.join("%20")}&response_type=token&show-dialog=true';
+export const loginurl=`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirecturi}&scope=${scopes.join("%20")}&response_type=code&show_dialog=true`;
+ console.log(loginurl);
